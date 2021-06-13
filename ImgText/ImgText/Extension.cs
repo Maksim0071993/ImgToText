e83896317f8d7d88c60b7d05d8a3310b26fb6ea3
Maksim Loginov <maksim08@list.ru>
2021-06-13 22:23:20 +0300

    init commit

---
+++ ImgText/ImgText/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgText
+{
+    public static class Extension
+    {
+        public static void ToGrayscale(this Bitmap bitmap)
+        {
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    var pixel = bitmap.GetPixel(j, i);
+                    int avg = (pixel.R + pixel.G + pixel.B) / 3;
+                    bitmap.SetPixel(j, i, Color.FromArgb(pixel.A, avg, avg, avg));
+                }
+            }
+        }
+    }
+}
---
+++ ImgText/ImgText/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+namespace ImgText
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 1.5;
+        private const int MAXWIDTH = 250;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images|*.bmp; *.png; *.jpg; *.JPEG"
+            };
+            Console.WriteLine(" Press enter to start");
+            while (true)
+            {
+                Console.ReadLine();
+               if(openFileDialog.ShowDialog()!=DialogResult.OK)
+                
+                    continue;
+                Console.Clear();
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+                var converter = new BitmapToAsciConverter(bitmap);
+                var rows = converter.Convert();
+                foreach ( var item in rows)
+                {
+                    Console.WriteLine(item);
+                    File.WriteAllLines("image.txt", rows.Select(r => new string(r)));
+                }
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+        private static Bitmap ResizeBitmap (Bitmap bitmap)
+        {
+            
+            var maxHeight = bitmap.Height / WIDTH_OFFSET * MAXWIDTH / bitmap.Width;
+            if (bitmap.Width> MAXWIDTH||bitmap.Height > maxHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MAXWIDTH, (int)maxHeight));
+            }
+            return bitmap;
+        }
+    }
+}
